504495f4a0d81e41278d8ab4f7301a563ee2efa1
Westermann <westermann93@googlemail.com>
2019-01-17 02:52:30 +0100

    OMG

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,7 +1,5 @@
-package bench
-
 import java.lang.NumberFormatException
-import org.apache.spark.sql.types.IntegerType
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 import scala.io.Source
 import scala.util.parsing.json.{JSON, JSONObject}
@@ -10,8 +8,6 @@ import scala.util.parsing.json.{JSON, JSONObject}
 class InputDataException(message: String) extends Exception(message)
 
 case class Codes(id: Int = -1, minutes_metric_1: Int = -1, minutes_metric_2: Int = -1)
-case class DataRow(Date: String, input_name: String, input_id: Int, level_name: String, level_id: Int, low_name: String, low_id: Int,
-                   activity_name: String, activity_id: Int, second_activity_id: Int, status: String, first_metric: Int, second_metric: Int)
 
 
 object BenchTestApp {
@@ -27,20 +23,19 @@ object BenchTestApp {
 
     implicit val spark: SparkSession = getSpark
 
-    val (valid, errored) = createOutput(vocabPath, dataPath)
-    writeOutput(valid)
+    val vocabJson = loadVocabularyJson(vocabPath)
+    val vocab = buildVocabularyDataFrame(vocabJson)
+    val data = loadData(dataPath)
+
+    val (valid, errored) = createOutput(vocab, data)
+
+    writeOutput(valid, outputCols, "output")
+    writeOutput(errored, data.columns, "errors")
 
     spark.stop
   }
 
-  def createOutput(vocabPath: String, dataPath: String)(implicit spark: SparkSession): Tuple2[DataFrame, DataFrame] = {
-    val vocabJson = loadVocabularyJson(vocabPath)
-    val vocab = buildVocabularyDataFrame(vocabJson)
-    /* println(vocab.show()) */
-
-    val data = loadData(dataPath)
-    /* println(data.show()) */
-
+  def createOutput(vocab: DataFrame, data: DataFrame)(implicit spark: SparkSession): Tuple2[DataFrame, DataFrame] = {
     var joined = data.join(vocab, data.col("activity_id") === vocab.col("id"), "left_outer").na.fill(-1)
     val minsPerDay = 1440
     val numDays = Seq(1, 7, 30, 90)
@@ -63,19 +58,12 @@ object BenchTestApp {
     (valid, errored)
   }
 
-  // def writeErrors(df: DataFrame): Unit = {
-  //   df.select(.map(df(_)): _*).repartition(1).write
-  //     .format("com.databricks.spark.csv")
-  //     .option("header", "true")
-  //     .save("src/main/resources/errors.csv")
-  // }
-
-  def writeOutput(df: DataFrame): Unit = {
+  def writeOutput(df: DataFrame, outputCols: Seq[String], name: String): Unit = {
     df.select(outputCols.map(df(_)): _*).repartition(1).write
       .format("com.databricks.spark.csv")
       .option("header", "true")
       .mode("overwrite")
-      .save("src/main/resources/output.csv")
+      .save(s"src/main/resources/$name.csv")
   }
 
   def loadData(dataPath: String)(implicit spark: SparkSession): DataFrame = {
@@ -88,22 +76,11 @@ object BenchTestApp {
 
   def buildVocabularyDataFrame(json: JSONObject)(implicit spark: SparkSession): DataFrame = { 
     import spark.implicits._
-    val csvRows: Array[String] = json.obj("data").toString().split("\n")
-    val csvData: Dataset[String] = spark.createDataset(csvRows)
-    val csvCodes = spark.read.option("header", "true").option("inferSchema", "true").csv(csvData).toDF().select("Codes").rdd.map {
-      row => row(0) match {
-        case string: String => {
-          val codeElements: Array[Int] = string.filterNot("() " contains _).split(";") map {
-            field => try { field.toInt } catch {
-              case e: NumberFormatException => -1
-            }
-          }
-          Codes(codeElements(0), codeElements(1), codeElements(2))
-        }
-        case _ => Codes()
-      }
-    }
-    spark.createDataFrame(csvCodes)
+    val csvCodes = spark.sparkContext.parallelize(json.obj("data").toString().split("\n"))
+      .map(_.split(",")(6).filterNot("() " contains _).split(";").padTo(3, ""))
+      .map(r => (r(0), r(1), r(2)))
+      .toDF("id", "minutes_metric_1", "minutes_metric_2")
+    csvCodes
   }
 
   def loadVocabularyJson(jsonPath: String): JSONObject = {
